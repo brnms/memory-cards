b21a48eccfac53e39bbaed5dfc21fb6dd0f9b600
Bruno Santos <bruno.matasantos@gmail.com>
2020-10-20 10:25:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ let currentActiveCard = 0;
 const cardsEl =  [];
 
 // Store card data
+const cardsData = getCardsData();
+
+/*
 const cardsData = [
   {
      question: 'What must a variable begin with?',
@@ -30,7 +33,7 @@ const cardsData = [
      question: 'Example of Case Sensitive Variable',
      answer: 'thisIsAVariable'
    }
-];
+];*/
 
 // Create all cards
 function createCards() {
@@ -75,6 +78,19 @@ function updateCurrentText() {
     currentEl.innerText = `${ currentActiveCard + 1 }/${cardsEl.length}`
 }
 
+// Get cards from local storage
+function getCardsData() {
+    const cards = JSON.parse(localStorage.getItem('cards'));
+    return cards === null ? [] : cards;
+}
+
+
+// Add card to local storage
+function setCardsData(cards) {
+    localStorage.setItem('cards', JSON.stringify(cards));
+    window.location.reload();
+}
+
 createCards();
 
 // Event listeners
@@ -104,4 +120,36 @@ prevBtn.addEventListener('click', () => {
     cardsEl[currentActiveCard].className = 'card active';
 
     updateCurrentText();
+})
+
+// Show add container
+showBtn.addEventListener('click', () => addContainer.classList.add('show'));
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
+
+// Add new card
+addCardBtn.addEventListener('click', () => {
+    const question = questionEl.value;
+    const answer = answerEl.value;
+    
+    if(question.trim() && answer.trim()){
+        const newCard = { question, answer };
+
+        createCard(newCard);
+
+        questionEl.value = '';
+        answerEl.value = '';
+
+        addContainer.classList.remove('show');
+
+        cardsData.push(newCard);
+
+        setCardsData(cardsData);
+    }
+});
+
+// Clear cards button
+clearBtn.addEventListener('click', () => {
+    localStorage.removeItem('cards')
+    cardsContainer.innerHTML = '';
+    window.location.reload();
 })
